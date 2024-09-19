773cf629405b219074aba4f90d01af2fb0adc406
ArthurClaro <0123456789arthur@gmail.com>
2024-09-19 02:15:41 -0300

    style: :zap: comments exclude

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,4 @@
 export const environment = {
-    api: 'https://us-central1-curso-de-angular-api.cloudfunctions.net/app/tasks',
-    // apiBooks: 'https://www.googleapis.com/books/v1/volumes?q=' + search + '&key=AIzaSyAnkLK7se_lJIeR47gyqoOgRnj82deARfk'
-    // apiBooks: 'https://www.googleapis.com/books/v1/volumes?q=flowers&filter=free-ebooks&key=AIzaSyAnkLK7se_lJIeR47gyqoOgRnj82deARfk'
-   
     apiBooksBaseUrl: 'https://www.googleapis.com/books/v1/volumes',
     apiBooksKey: 'AIzaSyAnkLK7se_lJIeR47gyqoOgRnj82deARfk'
 };
